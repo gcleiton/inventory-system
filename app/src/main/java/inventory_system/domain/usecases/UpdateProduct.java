39ace36b991731c8cc7fc4b0c432b5b9f0973c24
Gabriel Cleiton <ccgabrielcc2609@gmail.com>
2021-03-30 17:09:47 -0300

    doc: add Product docs

--- app/src/main/java/inventory_system/domain/usecases/UpdateProduct.java
+++ app/src/main/java/inventory_system/domain/usecases/UpdateProduct.java
@@ -5,5 +5,15 @@ import inventory_system.domain.entities.Entity;
 import java.sql.SQLException;
 
 public interface UpdateProduct {
+    /**
+     *
+     * Método que atualiza os dados do produto a partir do código.
+     * Além disso, verifica se houve mudança na quantidade e grava
+     * o devido registro na tabela de reports da quantidade
+     *
+     * @param code - Código do produto
+     * @param entity - Entidade do produto com os dados atualizados
+     * @throws SQLException
+     */
     public void update (String code, Entity entity) throws SQLException;
 }
